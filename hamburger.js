1cdf9ef500c6c58b2ab3eb3db37950c9940e469a
whoJuan <juanfamiliaaguirre@gmail.com>
2025-01-03 19:18:33 -0500

    Cambios en el menu hamburguesa

--- hamburger.js
+++ hamburger.js
@@ -3,15 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const navbar = document.getElementById("navbar");
     const closeNavbar = document.getElementById("close-navbar");
 
-    hamburger.addEventListener("click", function () {
-        navbar.classList.add("active");
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.add("active");
+        hamburger.style.display = "none";
         closeNavbar.style.display = "block";
     });
 
-    closeNavbar.addEventListener("click", function () {
-        navbar.classList.remove("active");
+    closeNavbar.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+        hamburger.style.display = "block";
         closeNavbar.style.display = "none";
     });
+});
 
     document.addEventListener("click", function (event) {
         if (!navbar.contains(event.target) && !hamburger.contains(event.target)) {
